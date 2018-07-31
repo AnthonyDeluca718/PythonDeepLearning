8546eee2d7e2d6f72bcc0757b62aa4d8117c5cab
AnthonyDeluca718 <billyshearsfan@aim.com>
2018-07-30 22:49:02 -0400

    actually working numpy implementation of digit recognition

---
+++ Basics/digit-classifier/main.py
@@ -0,0 +1,10 @@
+from collect import load_mnist
+from network import NeuralNetwork
+
+training_data, validation_data, test_data = load_mnist()
+net = NeuralNetwork([784, 16, 10])
+net.fit(list(training_data), list(validation_data))
+
+# champions:
+# [784, 16, 10]: 92.31
+# [784, 100, 16, 10]: 94.22
