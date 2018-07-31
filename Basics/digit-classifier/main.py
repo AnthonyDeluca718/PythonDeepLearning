288964bfd89c927ccc09ce7f41304273fd3feab8
Anthony <anthony@expandtheroom.com>
2018-07-31 11:16:14 -0400

    keras solver example added

--- Basics/digit-classifier/main.py
+++ Basics/digit-classifier/main.py
@@ -2,9 +2,10 @@ from collect import load_mnist
 from network import NeuralNetwork
 
 training_data, validation_data, test_data = load_mnist()
-net = NeuralNetwork([784, 16, 10])
+net = NeuralNetwork([784, 80, 40, 16, 10])
 net.fit(list(training_data), list(validation_data))
 
 # champions:
 # [784, 16, 10]: 92.31
 # [784, 100, 16, 10]: 94.22
+# [784, 80, 40, 16, 10]: 94.31
